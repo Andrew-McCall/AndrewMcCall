add31a4fa37523cf39dcc6b5d3b772cd83ca1cee
Andrew-McCall <andrewdavidmccall@hotmail.com>
2025-07-19 14:35:37 +0100

    fixed and uses tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import './styles.css'
 import watermark from './version.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-	<h1> WIP </h1>
-  </div>
+<div class="flex justify-center items-center min-h-screen bg-stone-950">
+  <h1 class="italic text-7xl font-bold bg-gradient-to-r from-green-500 via-green-700 to-green-900 bg-clip-text text-transparent text-center">
+    Andrew David McCall
+  </h1>
+</div>
+
 `
 
 watermark();
