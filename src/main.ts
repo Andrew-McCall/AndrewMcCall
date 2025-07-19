7ae93c7cbc9501861707e0b5a5b4cc489b54a030
Andrew-McCall <andrewdavidmccall@hotmail.com>
2025-07-19 14:39:51 +0100

    improvements for bots

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import './styles.css'
 import watermark from './version.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<div class="flex justify-center items-center min-h-screen bg-stone-950">
+<div class="flex justify-center items-center min-h-screen ">
   <h1 class="italic text-7xl font-bold bg-gradient-to-r from-green-500 via-green-700 to-green-900 bg-clip-text text-transparent text-center">
     Andrew David McCall
   </h1>
