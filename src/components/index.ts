63fda619a9204941982e4b72ae9f64bf24296509
Andrew-McCall <AndrewDavidMcCall@hotmail.com>
2025-10-10 10:10:14 +0100

    Fixed

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 export default () => {
-    return "<div class="flex justify-center items-center min-h-screen ">
+  return `<div class="flex justify-center items-center min-h-screen ">
   <h1 class="italic text-7xl font-bold bg-gradient-to-r from-green-500 via-green-700 to-green-900 bg-clip-text text-transparent text-center">
     Andrew David McCall
   </h1>
-</div>"
-}
+</div>`;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,27 @@
-import watermark from './version.ts'
+import watermark from "./version.ts";
 
-import index from 'components/index.ts'
+import index from "./components/index.ts";
 
-window.addEventListener('popstate', () => {
-    rednerPage()
+window.addEventListener("popstate", () => {
+  renderPage();
 });
 
-var app = document.querySelector<HTMLDivElement>('#app') 
-def renderPage(){
-    if (!app) {
-        return window.location.reload();
-    }
-   
-    app.innerHTML = "";   
-    const page = window.location.pathname.toLowerCase();
+var app = document.querySelector<HTMLDivElement>("#app");
+function renderPage() {
+  if (!app) {
+    return window.location.reload();
+  }
 
-    if (page === "/"){
-        app.innerHTML = index();
-    }else{
-        window.history.pushState({}, '', '/');
-    }
-    
-    watermark();
+  app.innerHTML = "";
+  const page = window.location.pathname.toLowerCase();
+
+  if (page === "/") {
+    app.innerHTML = index();
+  } else {
+    window.history.pushState({}, "", "/");
+  }
+
+  watermark();
 }
 
 renderPage();
-
-
