7ec7b47d7eb2f521370c8bd167b43d77673115e9
Andrew-McCall <andrewdavidmccall@hotmail.com>
2025-10-10 20:22:34 +0100

    padding

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ export default (app: HTMLElement) => {
   })
 
   app.innerHTML += `<div class="flex justify-center items-center min-h-screen ">
-  <h1 class="italic text-7xl font-bold bg-gradient-to-r from-green-500 via-green-700 to-green-900 bg-clip-text text-transparent text-center">
+  <h1 class="px-1 italic text-7xl font-bold bg-gradient-to-r from-green-500 via-green-700 to-green-900 bg-clip-text text-transparent text-center">
     Andrew David McCall
   </h1>
 </div>`;
